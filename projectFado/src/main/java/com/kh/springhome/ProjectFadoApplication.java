dee4a7d8c3b52906a0cd43fb6d344d6ca51e5496
nnn0008 <secondepect@gmail.com>
2023-09-05 16:40:45 +0900

    최초 업로드

---
+++ projectFado/src/main/java/com/kh/springhome/ProjectFadoApplication.java
@@ -0,0 +1,13 @@
+package com.kh.springhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectFadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectFadoApplication.class, args);
+	}
+
+}
